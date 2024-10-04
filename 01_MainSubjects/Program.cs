ebab5ecfd88a90ff46f5b98536acf6bf6c156720
Miyase Elif AKSOY <melif0661@gmail.com>
2024-10-04 18:42:27 +0300

    Add project files.

---
+++ 01_MainSubjects/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			#region Yazdırma Komutları
+			//Console.Write("Merhaba Dünya");
+
+			//Console.WriteLine("***** Yemek Kategorileri *****");
+			//Console.WriteLine();
+			//Console.WriteLine("1-Çorbalar");
+			//Console.WriteLine("2-Ana Yemekler");
+			//Console.WriteLine("3-Soğuk Başlangıçlar");
+			//Console.WriteLine("4-Salatalar");
+			//Console.WriteLine("5-Tatlılar");
+			//Console.WriteLine("6-İçecekler");
+			//Console.WriteLine("");
+			//Console.WriteLine("***** Yemek Kategorileri *****");
+
+			#endregion
+
+			#region Değişkenler
+
+			//string
+			//Değişken_türü değişken_adı
+			//string name;
+			//name = "Elif";
+			//Console.Write(name);
+
+			 
+			#endregion
+
+			Console.Read();
+		}
+	}
+}
+
+
+
+//Yazdırma Komutları
